1eb33d94f0c19ab3fc6efa61f87abb5d8981be06
Mindaugas Kapickas <mindaugas@mkp.com>
2021-06-01 09:56:03 +0300

    npm, express, ejs, index route, server running rdy4 mongo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 // npm init +
-
 // download express +
-
+// download ejs+
 // pasikurt pradini server
+const express = require('express');
+const app = express();
 
-// download ejs
+//sukuriam express app objekta
+
+// paleidzia serveri ir klausosi http ir kt requestu nurodytu portu
+app.listen(3000, () => console.log('server is running'));
 
 //add as view engine
-
-//create views folder
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+// pasileidziam serveri
+app.get('/', (req, res) => res.render('index'));
+//create views folder+
 
 //vienas index view kuri sugeneruojam
