f2eed581cfd4e4b7cc75bc1c75f22b5b137cfa89
Mindaugas Kapickas <mindaugas@mkp.com>
2021-06-01 13:19:36 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,49 +1,48 @@
-// npm init +
-// download express +
-//create views folder+
-// download ejs+
-// pasikurt pradini server
 const express = require('express');
 const app = express();
-// itraukiamas mongoose
+// isitraukiam mongoose
 const mongoose = require('mongoose');
-// istraukiamas mongoDB mano clusterio credentials
+
 const { mongoDbString } = require('./config/config');
 
 const Post = require('./models/post');
 
-// console.log('mongoDBstring:', mongoDbString);
-
-//add as view engine
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
-// prisijungimas prie DB
+// console.log(' mongoDbString', mongoDbString);
+// prisijungimas prie duomenu bazes
 mongoose
-  .connect(mongoDbString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(mongoDbString, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    console.log('Connected to Mongooooooose');
+    console.log('Conneced to Mongo ooooooooose');
     app.listen(3000);
   })
   .catch((err) => console.error(err.message));
 
-// paleidzia serveri ir klausosi http ir kt requestu nurodytu portu
-// app.listen(3000, () => console.log('server is running'));
-
-// pasileidziam serveri
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/posts', (req, res) => {
+  // get all posts
+  Post.find()
+    .then((result) => {
+      console.log(' result', result);
+      res.send(result);
+    })
+    .catch((err) => console.error(err.message));
+});
+
+// create new post
 app.get('/add-post', (req, res) => {
-  // sukuriam nauja posta pagal schemoj aprasyta modeli
+  // sukuriam nauja posta pagal schemoje aprasyta modeli
   const newPost = new Post({
-    title: 'This is Mongoose Shark Attack',
-    author: 'Mangustas Gustas Augustas',
-    body: 'Mongo db is an easy way to do databasing',
+    title: '3000 leagues unders sea',
+    author: 'Jaike Swim',
+    body: 'Mongo db is an easy way to db. and......',
   });
   // issaugoti duomenu bazeje naudojam .save()
-  newPost.save();
-  res.send('all good, maybe..');
+  newPost
+    .save()
+    .then((result) => res.send(result))
+    .catch((err) => console.error(err.message));
 });
